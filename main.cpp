e3d396121d70531357599ead71c6eb668fb0f100
Karris WEBB <153066380+KaraYaga@users.noreply.github.com>
2024-02-15 16:55:09 +0100

    [Collision With Edges]

--- main.cpp
+++ main.cpp
@@ -2,50 +2,76 @@
 #include <iostream>
 
 using namespace std;
+//BALL
+class Ball 
+{
+public:
+    float x, y;
+    int speedX, speedY;
+    int radius;
 
-//Initialize
+    //DRAW BALL
+    void Draw() 
+    {
+        DrawCircle(x, y, radius, WHITE);
+    }
+    //MOVE BALL
+    void Update() 
+    {
+        x += speedX;
+        y += speedY;
+
+        //Check Collisions
+        if (y + radius >= GetScreenHeight() || y - radius <= 0) 
+        {
+            speedY *= -1;
+        }
+        if (x + radius >= GetScreenWidth() || x - radius <= 0)
+        {
+            speedX *= -1;
+        }
+    }
+};
+
+//Ball Instance
+Ball ball;
+
+//INITIALIZE
 int main() {
+    
+    //Window State
+    const int screenWidth = 1280;
+    const int screenHeight = 800;
+    InitWindow(screenWidth, screenHeight, "PONG");
+    SetTargetFPS(60);
 
     //Font LIbrary
     Font ft = LoadFont("resources/fonts/pixantiqua.png");
 
-    //Ball Stats
-    int ballX = 100;
-    int ballY = 100;
-    int ballspeedX = 5;
-    int ballspeedY = 5;
-    int radius = 15;
-
-    //Window State
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-    InitWindow(screenWidth, screenHeight, "PONG");
-    SetTargetFPS(60);
+    ball.radius = 20;
+    ball.x = screenWidth / 2;
+    ball.y = screenHeight / 2;
+    ball.speedX = 7;
+    ball.speedY = 7;
 
     //Update Loop
     while (!WindowShouldClose()) 
     {
-        //Drawing Loop
+        //DRAWING LOOP
         BeginDrawing();
-        ClearBackground(BLACK);
-        //Title
-        DrawTextEx(ft, "PONGO BONGO GOGO GIRL!", Vector2{75, 15}, 20, 3, PINK);
 
-        //Ball
-        ballX += ballspeedX;
-        ballY += ballspeedY;
+        //UPDATE LOOP
+        ball.Update();
 
-        if (ballX + radius >= screenWidth  || ballX - radius <= screenHeight)
-        {
-            ballspeedX *= -1;
-        }
-        if ((ballY + radius >= GetScreenHeight()) || (ballY - radius <= 0))
-        {
-            ballspeedY *= -1;
-        }
-        DrawCircle(ballX, ballY, radius, PINK);
- 
-        //Finish Drawing
+        ClearBackground(BLACK);//Clears Console Background between frames
+        DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, WHITE);//Middle Line
+        ball.Draw();//Ball
+        DrawRectangle(20, screenHeight/2 - 60, 25, 120, PINK); //Left Paddle
+        DrawRectangle(screenWidth - 45, screenHeight / 2 - 60, 25, 120, PINK); //Right Paddle
+        DrawTextEx(ft, "PONGO BONGO GOGO GIRL!", Vector2{ 500, 15 }, 20, 3, PINK);//Title
+
+
+        //FINISH DRAWING
         EndDrawing();
     }
 
