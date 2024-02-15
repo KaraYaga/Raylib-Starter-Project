a08057aad73caacc59340282ad3d9403ba390910
Karris WEBB <153066380+KaraYaga@users.noreply.github.com>
2024-02-15 15:09:14 +0100

    [Commence]

--- main.cpp
+++ main.cpp
@@ -3,20 +3,46 @@
 
 using namespace std;
 
+//Initialize
 int main() {
 
-    
-    cout << "Hello World" << endl;
-
-    InitWindow(300, 300, "My first Raylib window!");
+    //Create Height, Width, Title of Window
+    InitWindow(400, 300, "My first Raylib window!");
     SetTargetFPS(60);
 
-    while (!WindowShouldClose()) {
+    //Ball Stats
+    int ballX = 150;
+    int ballY = 150;
+    int radius = 10;
+    int ballspeedX = 2;
+    int ballspeedY = 2;
+
+    //Update Loop
+    while (!WindowShouldClose()) 
+    {
+        ballX += ballspeedX;
+        ballY += ballspeedY;
+
+        if ((ballX + radius >= GetScreenWidth()) || (ballX - radius <= 0))
+        {
+            ballspeedX = -ballspeedX;
+        }
+        if ((ballY + radius >= GetScreenHeight()) || (ballY - radius <= 0))
+        {
+            ballspeedY = -ballspeedY;
+        }
+
+        //Drawing Loop
         BeginDrawing();
         ClearBackground(DARKGREEN);
+        //Draw Here
+        DrawCircle(ballX, ballY, 10, WHITE);
+        DrawText("Let's Play PONG!", 120, 20, 20, WHITE);
+
         EndDrawing();
     }
 
+    //End program and Clear
     CloseWindow();
     return 0;
 }
