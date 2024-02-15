7263cfe423cfffbbf1e5d18422b216d4a932f8b3
Karris WEBB <153066380+KaraYaga@users.noreply.github.com>
2024-02-15 17:23:00 +0100

    [Two Player No Score]

--- main.cpp
+++ main.cpp
@@ -33,7 +33,8 @@ public:
     }
 };
 
-class Paddle
+//Player Paddle
+class Paddle1
 {
 public:
     float x, y;
@@ -49,20 +50,67 @@ public:
     //MOVE PADDLE
     void Update() 
     {
-        if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_Z)) 
+        if (IsKeyDown(KEY_W)) 
         {
             y = y - speed;
         }
-        if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S))
+        if (IsKeyDown(KEY_S))
         {
             y = y + speed;
+        }
+        //Boundaries
+        if (y <= 0) 
+        {
+            y = 0;
+        }
+        if (y + height >= GetScreenHeight())
+        {
+            y = GetScreenHeight() - height;
+        }
+    }
+};
+
+//Player Paddle
+class Paddle2
+{
+public:
+    float x, y;
+    float width, height;
+    int speed;
+
+    //DRAW PADDLE
+    void Draw()
+    {
+        DrawRectangle(x, y, width, height, PINK);
+    }
+
+    //MOVE PADDLE
+    void Update()
+    {
+        if (IsKeyDown(KEY_UP))
+        {
+            y = y - speed;
+        }
+        if (IsKeyDown(KEY_DOWN))
+        {
+            y = y + speed;
+        }
+        //Boundaries
+        if (y <= 0)
+        {
+            y = 0;
+        }
+        if (y + height >= GetScreenHeight())
+        {
+            y = GetScreenHeight() - height;
         }
     }
 };
 
 //Instances
 Ball ball;
-Paddle player;
+Paddle1 player1;
+Paddle2 player2;
 
 //INITIALIZE
 int main() {
@@ -83,12 +131,20 @@ int main() {
     ball.speedX = 7;
     ball.speedY = 7;
 
-    //Paddle Player
-    player.width = 25;
-    player.height = 120;
-    player.x = screenWidth - player.width - 10;
-    player.y = screenHeight / 2 - player.height / 2;
-    player.speed = 6;
+    //Paddle Player1
+    player1.width = 25;
+    player1.height = 120;
+    player1.x = 20;
+    player1.y = screenHeight / 2 - player1.height / 2;
+    player1.speed = 6;
+
+    //Paddle Player2
+    player2.width = 25;
+    player2.height = 120;
+    player2.x = screenWidth - player2.width - 20;
+    player2.y = screenHeight / 2 - player2.height / 2;
+    player2.speed = 6;
+
 
     //Update Loop
     while (!WindowShouldClose()) 
@@ -98,12 +154,14 @@ int main() {
 
         //UPDATE LOOP
         ball.Update();
+        player1.Update();
+        player2.Update();
 
         ClearBackground(BLACK);//Clears Console Background between frames
         DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, WHITE);//Middle Line
         ball.Draw();//Ball
-        DrawRectangle(20, screenHeight/2 - 60, 25, 120, PINK); //Left Paddle
-        player.Draw();//Player
+        player1.Draw();//Player 1
+        player2.Draw();//Player 2
         DrawTextEx(ft, "PONGO BONGO GOGO GIRL!", Vector2{ 500, 15 }, 20, 3, PINK);//Title
 
 
